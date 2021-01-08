698762f22d200a19dd6291cee2d8f71e73149d0c
Nhocconsr <phamgiavang@gmail.com>
2021-01-09 08:48:13 +0900

    update vietcomic

--- vietcomic/src/home.js
+++ vietcomic/src/home.js
@@ -2,5 +2,6 @@ function execute() {
     return Response.success([
         { title: "Cập nhật", input: "https://vietcomic.net/truyen-tranh-hay?type=truyenmoi", script: "gen.js" },
         { title: "Truyện hot", input: "https://vietcomic.net/truyen-tranh-hay?type=hot", script: "gen.js" },
+        { title: "Siêu phẩm", input: "https://vietcomic.net/truyen-tranh-hay?type=sieu-pham", script: "gen.js" },
     ]);
 }
