c62eb42444dc8dec2deb6321cd90e07765d065bc
Nhocconsr <phamgiavang@gmail.com>
2021-01-16 20:29:42 +0900

    edit

--- truyenz/src/home.js
+++ truyenz/src/home.js
@@ -1,6 +1,8 @@
 function execute() {
     return Response.success([
         {title: "Cập nhật", input: "https://truyenz.info", script: "top.js"},
-        {title: "Manga", input: "https://truyenz.info/manga-genre/manga", script: "gen.js"}
+        {title: "Manga", input: "https://truyenz.info/manga-genre/manga", script: "gen.js"},
+        {title: "Manhwa", input: "https://truyenz.info/manga-genre/manhwa", script: "gen.js"},
+        {title: "Manhua", input: "https://truyenz.info/manga-genre/manhua", script: "gen.js"}
     ]);
 }
