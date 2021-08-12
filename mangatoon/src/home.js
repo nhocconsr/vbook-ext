52292e45a7a6e5d25c7a877e59ca763d9c036d44
PGV <46308607+nhocconsr@users.noreply.github.com>
2021-08-12 20:14:59 +0900

    remove mangatoon

--- mangatoon/src/home.js
+++
@@ -1,6 +0,0 @@
-function execute() {
-    return Response.success([
-        {title: "Cập nhật", input: "https://mangatoon.mobi/vi/genre/comic?type=1", script: "gen.js"},
-        {title: "Hoàn thành", input: "http://mangatoon.mobi/vi/genre/hot?type=1", script: "gen.js"},
-    ]);
-}
