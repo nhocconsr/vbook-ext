c72044b86be21697bd67c5bbaca779b1580d434f
PGV <46308607+nhocconsr@users.noreply.github.com>
2021-08-21 20:52:15 +0900

    remove

--- sangtacviet/src/home.js
+++
@@ -1,7 +0,0 @@
-function execute() {
-    return Response.success([
-        {title: "Mới nhất", input: "http://sangtacviet.com/?find=&minc=0&sort=update&tag=", script: "gen.js"},
-        {title: "Nhiều like", input: "http://sangtacviet.com/?find=&minc=0&sort=upvote&tag=", script: "gen.js"},
-        {title: "Xem nhiều", input: "http://sangtacviet.com/?find=&minc=0&sort=view&tag=", script: "gen.js"}
-    ]);
-}
