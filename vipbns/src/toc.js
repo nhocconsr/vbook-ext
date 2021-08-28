afab87b59dd3178d84e3d4aa629508040a013802
Son Thanh Nguyen <sontnguyen@kms-technology.com>
2021-08-28 17:35:01 +0700

    correct for loop to reach last page

--- vipbns/src/toc.js
+++ vipbns/src/toc.js
@@ -7,7 +7,7 @@ function execute(url) {
     var getChap = JSON.parse(getBNS);
     var lastChap = getChap['chapters_count'];
     const data = [];
-    for (let currentPage = 1; currentPage < Math.floor(lastChap / maxChapterPerPage) + 1; currentPage++) {
+    for (let currentPage = 1; currentPage <= Math.floor(lastChap / maxChapterPerPage) + 1; currentPage++) {
         const json = Http.get('https://api.bachngocsach.com/api/story/'+bookId+'/chapter').params({'per_page': maxChapterPerPage, 'page': currentPage}).string()
         var book = JSON.parse(json);
         var allChap = book.data;
