3a3af3ee360335ac4964fc2532311c08007011b2
PGV <46308607+nhocconsr@users.noreply.github.com>
2021-08-28 21:10:12 +0900

    fix load chap vipbns

---
+++ vipbns/src/page.js
@@ -0,0 +1,13 @@
+function execute(url) {
+    const bookId = url.split('/').pop().split('.').shift();
+    const GBns = Http.get('https://api.bachngocsach.com/api/story/'+ bookId).string();
+    var allPage = Math.round(JSON.parse(GBns).chapters_count/100) + 1;
+    var list = [];
+    if (GBns) {
+        for (var i = 1; i <= allPage; i++)
+            list.push("https://api.bachngocsach.com/api/story/"+bookId+"/chapter?per_page=100&page=" + i);
+        return Response.success(list);
+    }
+
+    return Response.success(list);
+}
--- vipbns/src/toc.js
+++ vipbns/src/toc.js
@@ -1,20 +1,18 @@
 function execute(url) {
-    const newUrl = url.replace('.html','/')
-    const bookId = url.split('/').pop().split('.')[0];
-    const getBNS = Http.get('https://api.bachngocsach.com/api/story/'+ bookId).string()
-    var getChap = JSON.parse(getBNS);
-    var lastChap = getChap['chapters_count'];
-    const json = Http.get('https://api.bachngocsach.com/api/story/'+bookId+'/chapter').params({'per_page': lastChap}).string()
-    var book = JSON.parse(json);
-    var allChap = book.data;
-    const data = [];
-    for (var i = 0; i < allChap.length; i++){
-        var chap = allChap[i]
-        data.push({
-            name: chap['name'],
-            url: newUrl+chap['slug']+'/'+chap['id']+'.html',
-            host: "https://vip.backngocsach.com"
-        })
+    const json = Http.get(url).string()
+    if (json){
+        var book = JSON.parse(json);
+        var allChap = book.data;
+        const data = [];
+        for (var i = 0; i < allChap.length; i++){
+            var chap = allChap[i]
+            data.push({
+                name: chap['name'],
+                url: chap['id'],
+                host: "https://vip.backngocsach.com"
+            })
+        }
+        return Response.success(data);
     }
-    return Response.success(data);
+    return Response.success(json)
 }
