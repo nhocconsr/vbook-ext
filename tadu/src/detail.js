f8d02b5e981f943729fa61673936420cb4598570
PGV <46308607+nhocconsr@users.noreply.github.com>
2021-09-07 23:10:49 +0900

    fix tadu

--- tadu/src/detail.js
+++ tadu/src/detail.js
@@ -1,11 +1,11 @@
 function execute(url) {
-    const doc = Http.get(url).html();
-
+    const nUrl = url.replace('m.','www.')
+    const doc = Http.get(nUrl).html();
     return Response.success({
         name: doc.select("a.bkNm").first().text(),
         cover: doc.select("a.bookImg img").first().attr("data-src"),
         author: doc.select(".bookNm span").first().text(),
-        description: doc.select(".bookIntro p").html(),
+        description: doc.select("p.intro").html(),
         detail: doc.select(".sortList a").first().html() + '<br>' +doc.select(".datum span").first().html(),
         host: "https://www.tadu.com",
     });
--- tadu/src/toc.js
+++ tadu/src/toc.js
@@ -1,10 +1,9 @@
 function execute(url) {
-    const bid = url.split('/')[4];
+    const bid = url.split('/').pop();
     function capitalize(s){
         return s[0].toUpperCase() + s.slice(1);
     }
     var doc = Http.get('https://www.tadu.com/book/catalogue/'+bid).html();
-
     var el = doc.select(".chapter").select("a");
     const data = [];
     for (var i = 0; i < el.size(); i++) {
