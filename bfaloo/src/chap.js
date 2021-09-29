ef35f01259ce06f9cfa34a3b4ba88b8b20c1554b
PGV <46308607+nhocconsr@users.noreply.github.com>
2021-09-30 05:10:07 +0900

    fix bfaloo

--- bfaloo/src/chap.js
+++ bfaloo/src/chap.js
@@ -1,8 +1,15 @@
 function execute(url) {
     var doc = Http.get(url).html();
-    doc.select(".noveContent p").last().remove();
     var htm = doc.select(".noveContent").html();
-    htm = htm.replace(/<a[^>]*>([^<]+)<\/a>/g,'');
-    htm = htm.replace(/&(nbsp|amp|quot|lt|gt);/g, "");
-    return Response.success(htm);
+    if (htm.match('您还没有登录，请登录后在继续阅读本部小说') != null) {
+        var content = 'Đây là chương VIP. Bạn vui lòng đăng nhập trang web và tự mua chương VIP nhé!<br>Nếu vẫn lỗi liên hệ email : phamgiavang@gmail.com hoặc Discord'
+        return Response.success(content)
+    }else{
+        doc.select(".noveContent p").last().remove();
+        var htm = doc.select(".noveContent").html();
+        htm = htm.replace(/<a[^>]*>([^<]+)<\/a>/g,'');
+        htm = htm.replace(/&(nbsp|amp|quot|lt|gt);/g, "");
+        return Response.success(htm);
+    }
+    
 }
