02c3606e9f6d2e04c31b2448e03b8af520ae7b53
PGV <46308607+nhocconsr@users.noreply.github.com>
2021-10-03 18:28:49 +0900

    fix doujins

--- doujins/src/genre.js
+++ doujins/src/genre.js
@@ -10,5 +10,15 @@ function execute() {
            script: 'gen.js'
         });
     }
+    const doc2 = Http.get("https://doujins.com/tags").html();
+    const el2 = doc2.select('#content .row .col-6 a');
+    for (var i = 0; i < el2.size(); i++) {
+        var e2 = el2.get(i);
+        data.push({
+           title: e2.text(),
+           input: e2.attr('href').replace('/',''),
+           script: 'gen.js'
+        });
+    }
     return Response.success(data);
 }
