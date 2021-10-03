c4c22e312959393d24dafbab3267f3d94b9f1f2f
PGV <46308607+nhocconsr@users.noreply.github.com>
2021-10-03 21:45:00 +0900

    fix

--- doujins/src/genre.js
+++ doujins/src/genre.js
@@ -1,15 +1,5 @@
 function execute() {
-    const doc = Http.get("https://doujins.com/series").html();
-    const el = doc.select('#content .row .col-6 a');
     const data = [];
-    for (var i = 0; i < el.size(); i++) {
-        var e = el.get(i);
-        data.push({
-           title: e.text(),
-           input: e.attr('href').replace('/',''),
-           script: 'gen.js'
-        });
-    }
     const doc2 = Http.get("https://doujins.com/tags").html();
     const el2 = doc2.select('#content .row .col-6 a');
     for (var i = 0; i < el2.size(); i++) {
