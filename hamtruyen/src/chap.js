e91a6024ec2cf6b290a20b9db7a340e808420e81
PGV <46308607+nhocconsr@users.noreply.github.com>
2021-10-03 15:59:14 +0900

    fix hamtruyen

--- hamtruyen/src/chap.js
+++ hamtruyen/src/chap.js
@@ -1,11 +1,17 @@
 function execute(url) {
     var doc = Http.get(url).html();
-    var el = doc.select("#content_chap img");
+    var check_lock = doc.text().match('Chương này đã bị khóa') != -1;
     var data = [];
-    for (var i = 0; i < el.size(); i++) {
-        var e = el.get(i);
-        data.push(e.attr("src"));
-        
+    if (check_lock === true){
+        var img = doc.select('#wrap_alertvip').select('img').attr('src');
+        data.push('https://hamtruyen.vn'+img)
+    }else{
+        var el = doc.select("#content_chap img");
+        for (var i = 0; i < el.size(); i++) {
+            var e = el.get(i);
+            data.push(e.attr("src"));
+            
+        }
     }
     return Response.success(data);
 }
