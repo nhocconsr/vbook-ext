38d4ca332d16e3244563e9a882793c8998474a55
PGV <46308607+nhocconsr@users.noreply.github.com>
2021-10-08 19:39:35 +0900

    fix fanqienovel

--- fanqienovel/src/home.js
+++ fanqienovel/src/home.js
@@ -1,7 +1,7 @@
 function execute() {
     return Response.success([
-        { title: "最新", input: "1", script: "gen.js" },
-        { title: "最热", input: "0", script: "gen.js" },
+        //{ title: "最新", input: "1", script: "gen.js" },
+        //{ title: "最热", input: "0", script: "gen.js" },
         { title: "都市榜", input: "1", script: "rank.js" },
         { title: "玄幻榜", input: "7", script: "rank.js" },
         { title: "小众榜", input: "-1", script: "rank.js" },
