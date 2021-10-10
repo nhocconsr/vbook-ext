87eb1f16f2f0c0694b84f6f118d3859fbe0e000a
PGV <46308607+nhocconsr@users.noreply.github.com>
2021-10-10 16:59:31 +0900

    fix truyengihot

--- truyengihot/src/chap.js
+++ truyengihot/src/chap.js
@@ -1,12 +1,13 @@
 function execute(url) {
+    const base = 'https://truyengihot.net/'
     var doc = Http.get(url).html();
     var el = doc.select(".pageWrapper img");
     const imgs = [];
     for (var i = 0; i < el.size(); i++) {
         var e = el.get(i);
-        var url = e.attr("data-echo");
-        if (!url.endsWith('donate.png')){
-            imgs.push(url)
+        var img = e.attr("data-echo");
+        if (!img.endsWith('donate.png')){
+            imgs.push(base+img)
         }
     }
     return Response.success(imgs);
