a749dde647546a53dbad7ec8677214e7be5b36ec
PGV <46308607+nhocconsr@users.noreply.github.com>
2021-10-30 01:06:48 +0900

    fix mangabuddy

--- mangabuddy/src/chap.js
+++ mangabuddy/src/chap.js
@@ -1,12 +1,12 @@
 function execute(url) {
     var doc = Http.get(url).html();
     var el = doc.select(".chapter-image img");
-    
     var data = [];
     for (var i = 0; i < el.size(); i++) {
         var e = el.get(i);
-        data.push(e.attr("data-src"));
-        
+        var img = e.attr("data-src");
+        if (img.startsWith('//')) img = img.replace('//','https://');
+        data.push(img);
     }
     return Response.success(data);
 }
--- mangabuddy/src/detail.js
+++ mangabuddy/src/detail.js
@@ -3,10 +3,10 @@ function execute(url) {
     return Response.success({
         name: doc.select(".name h1").text(),
         cover: doc.select(".img-cover img").first().attr('data-src').replace('//','https://'),
-        author: doc.select(".meta p > a").first().text(),
+        author: doc.select(".meta a[href~=author]").first().text(),
         description: doc.select(".section-body .content").text(),
-        detail: doc.select(".meta p").html(),
-        category: doc.select(".meta p").html(),
+        detail: 'Status : '+doc.select(".meta a[href~=status]").text()+'<br>Author : '+doc.select(".meta a[href~=author]").text(),
+        ongoing: doc.select(".meta a[href~=status]").first().text().indexOf("Ongoing") != -1,
         host: "https://mangabuddy.com"
     });
 }
