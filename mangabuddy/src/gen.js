f1997b540fae5f7ccf055bdc1335c7410bcf5dab
PGV <46308607+nhocconsr@users.noreply.github.com>
2021-11-21 03:10:17 +0900

    fix lỗi content :v:

--- mangabuddy/src/gen.js
+++ mangabuddy/src/gen.js
@@ -1,14 +1,11 @@
 function execute(url, page) {
     if (!page) page = '1';
     const doc = Http.get('https://mangabuddy.com'+url).params({page: page}).html();
-
     var next = doc.select(".paginator").select("a.active + a").text()
-
     const el = doc.select(".list .book-detailed-item")
-
     const data = [];
     for (var i = 0; i < el.size(); i++) {
-        var e = el.get(3);
+        var e = el.get(i);
         data.push({
             name: e.select("h3 a").first().text(),
             link: e.select("h3 a").first().attr("href"),
