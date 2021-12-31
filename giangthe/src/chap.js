e461ffed9f55597fc62f8ddf712ba0af2960a7c2
PGV <46308607+nhocconsr@users.noreply.github.com>
2022-01-01 00:58:19 +0900

    fix giangthe

--- giangthe/src/chap.js
+++ giangthe/src/chap.js
@@ -3,8 +3,8 @@ function execute(url) {
     if (response.ok) {
         let doc = response.html();
         let htm = doc.select(".contentbox p").html();
-
-        htm = htm.replace(/\n/g,'')
+        htm = htm.replace(/\n/g,'<br>')
+            .replace(/<\/?i.*?>/g,'')
             .replace(/&(nbsp|amp|quot|lt|gt);/g, "")
             .replace(/(<br\s*\/?>){2,}/g, '<br>'); 
         return Response.success(htm);
