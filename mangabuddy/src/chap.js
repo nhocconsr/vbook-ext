13e8236460ee9279b2daf5d18fe66716dcd66c2f
PGV <46308607+nhocconsr@users.noreply.github.com>
2022-01-16 21:55:22 +0900

    fix

--- mangabuddy/src/chap.js
+++ mangabuddy/src/chap.js
@@ -1,12 +1,12 @@
 function execute(url) {
-    const base = 'https://s1.mbcdnv1.xyz/file/img-mbuddy/manga/';
     let response = fetch(url);
     if(response.ok){
         let doc = response.text()
+        let server = doc.match('mainServer = "(.+)"')[1].replace('//','https://')
         let allImg = doc.match("chapImages =.'(.+)'")[1].split(',');
         let data = [];
-        allImg.forEach(item => data.push(base+item))
-        return Response.success(data);
+        allImg.forEach(item => data.push(server+item))
+        return Response.success(server);
     }
     return null;
 }
