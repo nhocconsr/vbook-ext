c7e28650e160c93f127092f0f4a19ad0376896f6
PGV <46308607+nhocconsr@users.noreply.github.com>
2022-01-16 21:56:18 +0900

    Update chap.js

--- mangabuddy/src/chap.js
+++ mangabuddy/src/chap.js
@@ -2,7 +2,7 @@ function execute(url) {
     let response = fetch(url);
     if(response.ok){
         let doc = response.text()
-        let server = doc.match('mainServer = "(.+)"')[1].replace('//','https://')
+        let server = doc.match('mainServer = "(.+)"')[1].replace('//','https://');
         let allImg = doc.match("chapImages =.'(.+)'")[1].split(',');
         let data = [];
         allImg.forEach(item => data.push(server+item))
