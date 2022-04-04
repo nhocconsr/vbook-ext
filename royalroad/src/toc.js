1570c7e7753b9958aea7a239ef11a5df31a12681
fusornat <96949286+fusornat@users.noreply.github.com>
2022-04-04 19:32:47 +0700

    Update toc.js

--- royalroad/src/toc.js
+++ royalroad/src/toc.js
@@ -3,7 +3,7 @@ function execute(url) {
 
     var el = doc.select(".portlet-body .chapter-row")
     const data = [];
-    for (var i = el.size() - 1; i >= 0; i--) {
+    for (var i = 0; i < el.size(); i++) {
         var e = el.get(i);
         data.push({
             name: e.select("a").first().text(),
