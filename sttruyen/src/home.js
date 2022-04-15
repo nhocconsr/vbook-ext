307835a3eed84f3a6257c226e74649ad113cc90e
PGV <46308607+nhocconsr@users.noreply.github.com>
2022-04-15 16:53:25 +0700

    fix

--- sttruyen/src/home.js
+++ sttruyen/src/home.js
@@ -3,6 +3,6 @@ function execute() {
         {title: "Truyện Dịch", input: "https://sttruyen.com/cat/truyen-dich", script: "gen.js"},
         {title: "Sáng Tác", input: "https://sttruyen.com/cat/truyen-sang-tac/", script: "gen.js"},
         {title: "Tiên Hiệp", input: "https://sttruyen.com/cat/tien-hiep/", script: "gen.js"},
-        {title: "Huyền Ảo", input: "https://sttruyen.com/cat/huyen-ao/", script: "gen.js"},
+        {title: "Kiếm Tu", input: "https://sttruyen.com/cat/kiem-tu", script: "gen.js"},
     ]);
 }
