0b36312073a8ae0c664fe16427b52846ada94738
PGV <46308607+nhocconsr@users.noreply.github.com>
2022-04-17 13:12:15 +0700

    update manhuagui

--- manhuagui/src/chap.js
+++ manhuagui/src/chap.js
@@ -1,23 +1,27 @@
 function execute(url) {
-    const html = Http.get(url).string();
-    const packed = html.match(/\(function\(p,a,c,k,e,d\).*?0,\{\}\)\)/)[0];
-    const marks = ["p;}('", "'['\\x73\\x70\\x6c\\x69\\x63']", 'SMH.imgData(', ').preInit();'];
-    var params = packed.substring(packed.indexOf(marks[0]) + marks[0].length, packed.indexOf(marks[1]));
-    var index0 = params.indexOf("',");
-	var index1 = params.indexOf(',', index0 + 2);
-	var index2 = params.indexOf(",'");
-	var param0 = params.substring(0, index0);
-	var param1 = parseInt(params.substring(index0 + 2, index1));
-	var param2 = parseInt(params.substring(index1 + 1, index2));
-	var param3 = params.substring(index2 + 2);
-    var unpacked = unPacker(param0, param1, param2, LZString.decompressFromBase64(param3).split('|'), 0, {});
-    var result = JSON.parse(unpacked.substring(unpacked.indexOf(marks[2]) + marks[2].length,unpacked.indexOf(marks[3])));
-    const resHost = 'https://i.hamreus.com'+ result.path;
-    const vefi = '?e='+result.sl.e+'&m='+result.sl.m;
-    const data = result.files.map((s) => {
-        return  resHost+s+vefi
-    });
-    return Response.success(data); 
+    let response = fetch(url);
+    if(response.ok){
+        let html = response.text();
+        let packed = html.match(/\(function\(p,a,c,k,e,d\).*?0,\{\}\)\)/)[0];
+        let marks = ["p;}('", "'['\\x73\\x70\\x6c\\x69\\x63']", 'SMH.imgData(', ').preInit();'];
+        var params = packed.substring(packed.indexOf(marks[0]) + marks[0].length, packed.indexOf(marks[1]));
+        var index0 = params.indexOf("',");
+        var index1 = params.indexOf(',', index0 + 2);
+        var index2 = params.indexOf(",'");
+        var param0 = params.substring(0, index0);
+        var param1 = parseInt(params.substring(index0 + 2, index1));
+        var param2 = parseInt(params.substring(index1 + 1, index2));
+        var param3 = params.substring(index2 + 2);
+        var unpacked = unPacker(param0, param1, param2, LZString.decompressFromBase64(param3).split('|'), 0, {});
+        var result = JSON.parse(unpacked.substring(unpacked.indexOf(marks[2]) + marks[2].length,unpacked.indexOf(marks[3])));
+        const resHost = 'https://i.hamreus.com'+ result.path;
+        const vefi = '?e='+result.sl.e+'&m='+result.sl.m;
+        const data = result.files.map((s) => {
+            return  resHost+s+vefi
+        });
+        return Response.success(data); 
+    }
+
 }
 var LZString=(function(){var f=String.fromCharCode;var keyStrBase64="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";var baseReverseDic={};function getBaseValue(alphabet,character){if(!baseReverseDic[alphabet]){baseReverseDic[alphabet]={};for(var i=0;i<alphabet.length;i++){baseReverseDic[alphabet][alphabet.charAt(i)]=i}}return baseReverseDic[alphabet][character]}var LZString={decompressFromBase64:function(input){if(input==null)return"";if(input=="")return null;return LZString._0(input.length,32,function(index){return getBaseValue(keyStrBase64,input.charAt(index))})},_0:function(length,resetValue,getNextValue){var dictionary=[],next,enlargeIn=4,dictSize=4,numBits=3,entry="",result=[],i,w,bits,resb,maxpower,power,c,data={val:getNextValue(0),position:resetValue,index:1};for(i=0;i<3;i+=1){dictionary[i]=i}bits=0;maxpower=Math.pow(2,2);power=1;while(power!=maxpower){resb=data.val&data.position;data.position>>=1;if(data.position==0){data.position=resetValue;data.val=getNextValue(data.index++)}bits|=(resb>0?1:0)*power;power<<=1}switch(next=bits){case 0:bits=0;maxpower=Math.pow(2,8);power=1;while(power!=maxpower){resb=data.val&data.position;data.position>>=1;if(data.position==0){data.position=resetValue;data.val=getNextValue(data.index++)}bits|=(resb>0?1:0)*power;power<<=1}c=f(bits);break;case 1:bits=0;maxpower=Math.pow(2,16);power=1;while(power!=maxpower){resb=data.val&data.position;data.position>>=1;if(data.position==0){data.position=resetValue;data.val=getNextValue(data.index++)}bits|=(resb>0?1:0)*power;power<<=1}c=f(bits);break;case 2:return""}dictionary[3]=c;w=c;result.push(c);while(true){if(data.index>length){return""}bits=0;maxpower=Math.pow(2,numBits);power=1;while(power!=maxpower){resb=data.val&data.position;data.position>>=1;if(data.position==0){data.position=resetValue;data.val=getNextValue(data.index++)}bits|=(resb>0?1:0)*power;power<<=1}switch(c=bits){case 0:bits=0;maxpower=Math.pow(2,8);power=1;while(power!=maxpower){resb=data.val&data.position;data.position>>=1;if(data.position==0){data.position=resetValue;data.val=getNextValue(data.index++)}bits|=(resb>0?1:0)*power;power<<=1}dictionary[dictSize++]=f(bits);c=dictSize-1;enlargeIn--;break;case 1:bits=0;maxpower=Math.pow(2,16);power=1;while(power!=maxpower){resb=data.val&data.position;data.position>>=1;if(data.position==0){data.position=resetValue;data.val=getNextValue(data.index++)}bits|=(resb>0?1:0)*power;power<<=1}dictionary[dictSize++]=f(bits);c=dictSize-1;enlargeIn--;break;case 2:return result.join("")}if(enlargeIn==0){enlargeIn=Math.pow(2,numBits);numBits++}if(dictionary[c]){entry=dictionary[c]}else{if(c===dictSize){entry=w+w.charAt(0)}else{return null}}result.push(entry);dictionary[dictSize++]=w+entry.charAt(0);enlargeIn--;w=entry;if(enlargeIn==0){enlargeIn=Math.pow(2,numBits);numBits++}}}};return LZString})();String.prototype.splic=function(f){return LZString.decompressFromBase64(this).split(f)};
 const unPacker = function(p, a, c, k, e, d) {
--- manhuagui/src/genre.js
+++ manhuagui/src/genre.js
@@ -1,14 +1,18 @@
 function execute() {
-    const doc = Http.get("https://www.manhuagui.com").html();
-    const el = doc.select(".index-cont li a");
-    const data = [];
-    for (var i = 0; i < el.size(); i++) {
-        var e = el.get(i);
-        data.push({
-           title: e.text(),
-           input: e.attr('href').split('/')[2]+"/index",
-           script: 'gen.js'
-        });
+    let response = fetch("https://www.manhuagui.com");
+    if(response.ok){
+        let doc = response.html();
+        let el = doc.select(".index-cont li a");
+        let data = [];
+        for (var i = 0; i < el.size(); i++) {
+            var e = el.get(i);
+            data.push({
+            title: e.text(),
+            input: e.attr('href').split('/')[2]+"/index",
+            script: 'gen.js'
+            });
+        }
+        return Response.success(data);
     }
-    return Response.success(data);
+    return null;
 }
