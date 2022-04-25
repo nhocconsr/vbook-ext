380b748020a29d795a764343a5197bfae28a60f9
PGV <46308607+nhocconsr@users.noreply.github.com>
2022-04-25 23:30:56 +0700

    update

--- hamtruyen/src/home.js
+++ hamtruyen/src/home.js
@@ -1,8 +1,10 @@
 function execute() {
     return Response.success([
-        { title: "Cập Nhật", input: "2", script: "gen.js" },
-        { title: "Bảng Xếp Hạng", input: "1", script: "gen.js" },
-        { title: "Truyện Mới", input: "3", script: "gen.js" },
-        { title: "Hoàn Thành", input: "2", script: "source.js" },
+        { title: "Cập Nhật (Tranh)", input: "2", script: "gen.js" },
+        { title: "Cập Nhật (Chữ)", input: "2", script: "gen2.js" },
+        { title: "Bảng Xếp Hạng (Tranh)", input: "1", script: "gen.js" },
+        { title: "Bảng Xếp Hạng (Chữ)", input: "1", script: "gen2.js" },
+        { title: "Truyện Mới (Tranh)", input: "3", script: "gen.js" },
+        { title: "Truyện Mới (Chữ)", input: "3", script: "gen2.js" },
     ]);
 }
