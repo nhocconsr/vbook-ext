448119f285095237e6d87b62e209ccda8dc0b085
PGV <46308607+nhocconsr@users.noreply.github.com>
2022-04-28 10:43:53 +0700

    update

--- vlogtruyen/src/toc.js
+++ vlogtruyen/src/toc.js
@@ -1,16 +1,28 @@
 function execute(url) {
-    var doc = Http.get(url).html();
-
-    var el = doc.select(".ul-list-chaper-detail-commic li a")
-    const data = [];
-    for (var i = el.size() - 1; i >= 0; i--) {
-        var e = el.get(i);
-        data.push({
-            name: e.select("h3").text(),
-            url: e.attr("href"),
-            host: "https://vlogtruyen.net"
+    let response = fetch(url);
+    if(response.ok){
+        let doc = response.html();
+        let mid = doc.select('input[name=manga_id]').attr('value');
+        let res = fetch("https://vlogtruyen.net/thong-tin-ca-nhan?manga_id="+mid, {
+            method: "GET",
+            headers: {
+                "x-requested-with": "XMLHttpRequest"
+            }
         })
+        if(res.ok){
+            let allChap = Html.parse(res.json().data.chaptersHtml);
+            let el = allChap.select("li a")
+            let list = [];
+            for (var i = el.size() - 1; i >= 0; i--) {
+                var e = el.get(i);
+                list.push({
+                    name: e.select("h3").text(),
+                    url: e.attr("href"),
+                    host: "https://vlogtruyen.net"
+                })
+            }
+            return Response.success(list);
+        }
     }
-
-    return Response.success(data);
+    return null;
 }
