f0e45a22ea9e6b65c2ccc8ec1fad1845c75c59a7
PGV <nhocconsr@gmail.com>
2022-05-28 22:00:51 +0700

    fix

--- khotruyentranh/src/home.js
+++ khotruyentranh/src/home.js
@@ -1,8 +1,8 @@
 function execute() {
     return Response.success([
-        {title: "Cập nhật", input: "https://khotruyentranhz.com/danh-sach-truyen/", script: "gen.js"},
-        {title: "Ngôn tình", input: "https://khotruyentranhz.com/the-loai/ngon-tinh", script: "cat.js"},
-        {title: "Đam mỹ", input: "https://khotruyentranhz.com/the-loai/dam-my", script: "cat.js"},
-        {title: "Manhwa", input: "https://khotruyentranhz.com/the-loai/manhwa", script: "cat.js"},
+        {title: "Cập nhật", input: "https://khotruyentranhfull.com/danh-sach-truyen/", script: "gen.js"},
+        {title: "Ngôn tình", input: "https://khotruyentranhfull.com/the-loai/ngon-tinh", script: "cat.js"},
+        {title: "Đam mỹ", input: "https://khotruyentranhfull.com/the-loai/dam-my", script: "cat.js"},
+        {title: "Manhwa", input: "https://khotruyentranhfull.com/the-loai/manhwa", script: "cat.js"},
     ]);
 }
