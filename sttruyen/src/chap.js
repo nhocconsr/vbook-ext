b7ce6b426048e260dd53097df8478b4e7b6804e8
PGV <nhocconsr@gmail.com>
2022-06-01 21:33:10 +0700

    fix sttruyen

--- sttruyen/src/chap.js
+++ sttruyen/src/chap.js
@@ -1,26 +1,12 @@
+load('base64.js');
 function execute(url) {
-    let response = fetch(url);
+    let doc = fetch(url).html();
+    let cid = doc.select('reading-component').attr('chapter_id');
+    let response = fetch('https://sttruyen.com/web/reading/chapter/'+cid)
     if(response.ok){
-        let doc = response.html();
-        let csrf = doc.select('meta[name=csrf-token]').attr('content')
-        let rest = doc.select('div[wire:id]').attr('wire:initial-data');
-        let data1 = JSON.parse(rest)
-        var finger = data1.fingerprint
-        var serverMemo = data1.serverMemo
-        let res2 = fetch("https://sttruyen.com/livewire/message/reading", {
-            "method": "POST",
-            "headers": {
-                "Content-Type": "application/json",
-                "X-CSRF-TOKEN": csrf,
-            },
-            "body": "{\"fingerprint\":"+JSON.stringify(finger)+",\"serverMemo\":"+JSON.stringify(serverMemo)+",\"updates\":[{\"type\":\"callMethod\",\"payload\":{\"id\":\"4aqu\",\"method\":\"loadContent\",\"params\":[]}}]}"
-        });
-        let data = res2.json();
-        let content = data.effects.emits[0].params[0]
-            .replace(/\r\n/g, "")
-            .replace(/(<br ?\/?>\s*){2,}/gm, '<br>');
-        return Response.success(content);
-
+        let data = response.json();
+        let encrypted = data.chapter.content;
+        return Response.success(Base64.decode(encrypted));
     }
     return null;
 }
