486bb91eab9e9f49025c52d6ed4d7a3377255bce
fusornat <fusornat@gmail.com>
2022-06-10 16:22:54 +0700

    update home.js

--- truyenmoii/src/home.js
+++ truyenmoii/src/home.js
@@ -1,6 +1,7 @@
 function execute(url) {
     const doc = Http.get("https://truyenmoii.com/").html()
-    el = doc.select("ul.dropdown-menu li a")
+    var firstList = doc.select("ul.dropdown-menu").first()
+    var el = firstList.select('li a')
     const data = []
     for (var i = 0; i < 4; i++) {
         var e = el.get(i);
