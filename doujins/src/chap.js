a0aa5bd557540325d03a0a4eff1a05a0f1caed28
PGV <nhocconsr@gmail.com>
2022-09-01 23:28:12 +0700

    fix

--- doujins/src/chap.js
+++ doujins/src/chap.js
@@ -2,7 +2,7 @@ function execute(url) {
     var doc = Http.get(url).html();
     var el = doc.select('.swiper-wrapper img');
     var list = []
-    el.forEach(element => {
+    el.forEach(e => {
         list.push(e.attr('data-src'))
     });
     return Response.success(list)
