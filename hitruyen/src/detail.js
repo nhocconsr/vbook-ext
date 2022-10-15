37d9406f617677d78c8a39f6f705c2489394fc5a
fusornat <fusornat@gmail.com>
2022-10-15 23:42:45 +0700

    fix something

--- hitruyen/src/detail.js
+++ hitruyen/src/detail.js
@@ -2,7 +2,7 @@ function execute(url) {
     const doc = Http.get(url).html()
     return Response.success({
         name: doc.select("h1").text(),
-        cover: doc.select(".summary_image img").first().attr('srcset'),
+        cover: doc.select(".summary_image img").first().attr('src'),
         description: doc.select(".description-summary").text(),
         detail: doc.select(".post-content").html(),
         category: doc.select(".genres-content").html(),
