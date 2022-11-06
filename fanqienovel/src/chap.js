1cacbc69d0a0ea57b876a08246808d089d3a2f1d
dat-bi <83175246+dat-bi@users.noreply.github.com>
2022-11-06 10:42:17 +0700

    fix fanqienovel
    fix fanqienovel

--- fanqienovel/src/chap.js
+++ fanqienovel/src/chap.js
@@ -1,8 +1,18 @@
 function execute(url) {
-    const idChap = url.split('/').pop();
-    var json = Http.get('https://novel.snssdk.com/api/novel/book/reader/full/v1/?item_id='+idChap).string();
-    var data = JSON.parse(json).data.content;
-    var doc = Html.parse(data);
-    var content = doc.select('article').html();
-    return Response.success(content);
+    let chapid = url.split(/[/ ]+/).pop()
+    let newurl = "https://novel.snssdk.com/api/novel/book/reader/full/v1/?group_id=7130996041780593165&item_id=" + chapid + "&aid=1976"
+    let response = fetch(newurl, {
+        headers: {
+            'user-agent': UserAgent.android()
+        }
+    });
+    if (response.ok) {
+        let res_json = response.json();
+        console.log(res_json)
+        let dataa = res_json.data.content;  
+        var doc = Html.parse(dataa);
+        var content = doc.select('article').html();
+        return Response.success(content);
+    }
+    return null;
 }
