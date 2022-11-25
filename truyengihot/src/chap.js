9e447f229ec92ff136ca3116d7cee3934543726d
PGV <nhocconsr@gmail.com>
2022-11-25 09:18:29 +0700

    fix lỗi load chap

--- truyengihot/src/chap.js
+++ truyengihot/src/chap.js
@@ -1,20 +1,41 @@
 function execute(url) {
-    const base = 'https://truyengihotne.net'
-    let doc = fetch(url).html();
-    let el = doc.select(".pageWrapper img");
-    let imgs = [];
-    for (let i = 0; i < el.size(); i++) {
-        let e = el.get(i);
-        let img = e.attr("data-echo") || e.attr("src");
-        if(img.indexOf('donate') <= 0){
-            if(img.startsWith('/')){
-                imgs.push(base + img)
-            }else if(img.startsWith('upload')){
-                imgs.push(base +'/'+ img)
-            }else{
-                imgs.push(img)
+    const base = 'https://truyengihotne.net';
+    let response = fetch(url);
+    if(response.ok){
+        let gtext = response.text();
+        var mangaID = gtext.match('mangaID = "(.+)"')[1];
+        var mangaSLUG = gtext.match('mangaSLUG = "(.+)"')[1];
+        var g_id = gtext.match('g_id = "(.+)"')[1];
+        var cid = gtext.match('cid = "(.+)"')[1];
+        var chapter = gtext.match('chapter = "(.+)"')[1];
+        var token = gtext.match('_token = "(.+)"')[1];
+        let gimg = fetch("https://truyengihotne.net/frontend_controllers/chapter/content.php", {
+            method : "POST",
+            body: {
+                token: token,
+                chapter_id: cid,
+                m_slug: mangaSLUG,
+                m_id: mangaID,
+                chapter: chapter,
+                g_id: g_id,
+            }
+        }).json().content
+        let el = Html.parse(gimg).select(".page img");
+        let imgs = [];
+        for (let i = 0; i < el.size(); i++) {
+            let e = el.get(i);
+            let img = e.attr("data-echo") || e.attr("src");
+            if(img.indexOf('default') <= 0){
+                if(img.startsWith('/')){
+                    imgs.push(base + img)
+                }else if(img.startsWith('upload')){
+                    imgs.push(base +'/'+ img)
+                }else{
+                    imgs.push(img)
+                }
             }
         }
+        return Response.success(imgs);
     }
-    return Response.success(imgs);
+    return Response.success('https://github.com/nhocconsr/vbook-ext/blob/46879e15852dad5171c65a77e3ac2950bbd58a80/huongdan/taoanhdep_cover_chuky2_18236.jpg');
 }
