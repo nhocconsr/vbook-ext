35603439c67f107553975d77fe42dbc790537fa7
PGV <nhocconsr@gmail.com>
2023-01-27 21:46:49 +0700

    fix truyencool

--- truyencool/src/toc.js
+++ truyencool/src/toc.js
@@ -1,20 +1,19 @@
 function execute(url) {
-    var doc = Http.get(url).html();
-    var bookId = doc.select('#manga-chapters-holder').attr('data-id');
-    const gdata = Http.post('https://truyencool.net/wp-admin/admin-ajax.php').params({
-        'action': 'manga_get_chapters',
-        'manga': bookId
-    }).html();
-    gdata.select('.c-new-tag').remove();
-    var el = gdata.select(".listing-chapters_wrap li a");
-    const listChap = [];
-    for (var i = el.size() - 1; i >= 0; i--) {
-        var e = el.get(i);
-        listChap.push({
-            name: e.text(),
-            url: e.attr("href"),
-            host: "https://truyencool.net"
-        })
+    let response = fetch(url);
+    if (response.ok){
+        var doc = response.html();
+        doc.select('.c-new-tag').remove();
+        var el = doc.select(".version-chap li a");
+        const listChap = [];
+        for (var i = el.size() - 1; i >= 0; i--) {
+            var e = el.get(i);
+            listChap.push({
+                name: e.text(),
+                url: e.attr("href"),
+                host: "https://truyencool.net"
+            })
+        }
+        return Response.success(listChap);
     }
-    return Response.success(listChap);
+    
 }
--- truyenhaytv/src/home.js
+++
@@ -1,7 +0,0 @@
-function execute() {
-    return Response.success([
-        {title: "Cập Nhật", input: "/truyen-moi-cap-nhat.html", script: "gen.js"},
-        {title: "Top Tuần", input: "/top-tuan.html", script: "gen.js"},
-        {title: "Truyện Mới", input: "/truyen-tranh-moi.html", script: "gen.js"},
-    ]);
-}
