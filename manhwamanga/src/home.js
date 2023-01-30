1325c803859414ca91b83da35461cd5fdceed563
PGV <nhocconsr@gmail.com>
2023-01-30 12:04:03 +0700

    xoa site loi

--- manhwamanga/src/home.js
+++
@@ -1,7 +0,0 @@
-function execute() {
-    return Response.success([
-        { title: "Lastest", input: "https://manhwamanga.net/latest-updates", script: "gen.js" },
-        { title: "New manga", input: "https://manhwamanga.net/new", script: "gen.js" },
-        { title: "Completed", input: "https://manhwamanga.net/completed", script: "gen.js" },
-    ]);
-}
--- readmanhwa/src/home.js
+++
@@ -1,6 +0,0 @@
-function execute() {
-    return Response.success([
-        {title: "Lastest", input: "https://readmanhwa.com/api/comics", script: "gen.js"},
-        {title: "Popular", input: "https://readmanhwa.com/api/comics", script: "top.js"},
-    ]);
-}
