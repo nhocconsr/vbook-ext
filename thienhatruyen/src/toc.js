2687d2885fb582fc602f6ce71f34e9776c5c3821
PGV <nhocconsr@gmail.com>
2023-01-30 12:23:05 +0700

    fix thienhatruyen loi chapter

--- thienhatruyen/src/toc.js
+++ thienhatruyen/src/toc.js
@@ -1,14 +1,25 @@
 function execute(url) {
-    var doc = Http.get(url).html();
-    var el = doc.select(".listChapters .list a")
-    const data = [];
-    for (var i = el.size() - 1; i >= 0; i--) {
-        var e = el.get(i);
-        data.push({
-            name: e.select(".titleComic").text(),
-            url: e.attr("href"),
-            host: "https://thienhatruyen.com"
-        })
+    let mid = url.split('-').pop().split('.').shift();
+    const options = {
+        method: 'GET',
+        headers: {
+            'x-requested-with': 'XMLHttpRequest'
+        }
+    };
+    let response = fetch('https://thienhatruyen.com/thong-tin-ca-nhan?manga_id='+mid, options);
+    if (response.ok){
+        var doc = Html.parse(response.json().data.chaptersHtml);
+        var el = doc.select("li a")
+        const data = [];
+        for (var i = el.size() - 1; i >= 0; i--) {
+            var e = el.get(i);
+            data.push({
+                name: e.select(".titleComic").text(),
+                url: e.attr("href"),
+                host: "https://thienhatruyen.com"
+            })
+        }
+        return Response.success(data);
     }
-    return Response.success(data);
+    return Response.error('message') // Trả về response thất bại với nội dung lỗi
 }
