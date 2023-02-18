7afd3b86746360fe5a16619e9b5a0787b7e7f77c
dat-bi <83175246+dat-bi@users.noreply.github.com>
2023-02-18 19:02:16 +0700

    fix search fanqienovel

--- fanqienovel/src/search.js
+++ fanqienovel/src/search.js
@@ -1,27 +1,22 @@
-function execute(key, page) {
-    const uPic = 'https://p3-tt.byteimg.com/img/';
-    if (!page) page = '0';
-    var json = Http.get('https://fanqienovel.com/api/author/search/search_book/v1')
-        .params({
-            filter: '127,127,127',
-            page_count : '18',
-            page_index : page,
-            query_type : 0,
-            query_word : key
-        })
-        .string();
-    if(json){
-        var data = JSON.parse(json).data
-        var allBook = data.search_book_data_list
+function execute(key) {    
+    let response = fetch('https://api3-normal-lf.fqnovel.com/reading/bookapi/search/tab/v/?offset=0&passback=&query='+ key +'&search_id=&iid=308323375917453&aid=1967&app_name=novelapp&version_code=504&version_name=5.0.4.32&device_platform=android', {
+        headers: {
+            'user-agent': UserAgent.android()
+        }
+    });
+    if (response.ok) {
+        let res_json = response.json();
+        let allBook = res_json.search_tabs[0].data;
         const book = [];
         for (var i = 0; i < allBook.length; i++) {
-            var item = allBook[i]
-            book.push({
-                name: item['book_name'],
-                link: "https://fanqienovel.com" + 'page/'+item['book_id'],
-                cover: uPic+item['thumb_uri']+'~180x234.jpg',
-                description: item['author'],
-                host: "https://fanqienovel.com"
+            if(i==3){i++}
+            let item = allBook[i];
+            book.push({ 
+                name: item.book_data[0].original_book_name,
+                description: item.book_data[0].author,
+                cover: item.book_data[0].thumb_url,               
+                link: "https://api3-normal-lf.fqnovel.com/reading/bookapi/directory/all_items/v/?need_version=true&book_id=" +item.book_data[0].book_id + "&iid=2665637677906061&aid=1967&app_name=novelapp&version_code=495",
+                host: "https://api3-normal-lf.fqnovel.com"
             })
         }
         return Response.success(book)      
