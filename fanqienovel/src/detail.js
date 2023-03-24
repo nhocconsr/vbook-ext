fb0a3e0cc7d30c9836bc8471e77d988282265216
dat-bi <83175246+dat-bi@users.noreply.github.com>
2023-03-24 21:15:45 +0700

    fix  detail fanqie

--- fanqienovel/src/detail.js
+++ fanqienovel/src/detail.js
@@ -1,11 +1,6 @@
 function execute(url) {
-    if (url.includes("fanqienovel")) {
-        var bookid = url.match(/\d+/g)[0]
-    }
-    if (url.includes("fqnovel")) {
-        bookid = url.match(/\d+/g)[1]
-    }
-    let newurl = "https://api3-normal-lf.fqnovel.com/reading/bookapi/directory/all_items/v/?need_version=true&book_id=" + bookid + "&iid=2159861899465991&aid=1967&app_name=novelapp&version_code=311"
+    var bookid = url.replace(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+)/img, "").match(/\d+/g)[0]
+    let newurl = "https://api5-normal-lf.fqnovel.com/reading/bookapi/detail/v/?book_id="+bookid+"&iid=2159861899465991&aid=1967&version_code=311&update_version_code=31132"
     let response = fetch(newurl, {
         headers: {
             'user-agent': UserAgent.android()
@@ -13,19 +8,20 @@ function execute(url) {
     });
     if (response.ok) {
         let res_json = response.json();
-        const last_chapter_update_time = res_json.data.book_info.last_chapter_update_time
+        const last_chapter_update_time = res_json.data.last_chapter_update_time
         const last_chapter_update_time_string = new Date(last_chapter_update_time * 1000).toLocaleDateString("vi-VN", {
             day: "2-digit",
             month: "2-digit",
             year: "2-digit"
         });
-        let authorInfo = res_json.data.book_info.original_authors;
-        let detailinfo = res_json.data.book_info.sub_info;
+        console.log(last_chapter_update_time_string)
+        let authorInfo = res_json.data.original_authors;
+        let detailinfo = res_json.data.sub_info;
         let author = JSON.parse(authorInfo)[0].AuthorName;
-        let bookname = res_json.data.book_info.original_book_name;
-        let bookid = res_json.data.book_info.book_id;
-        let description = res_json.data.book_info.abstract.replace(/\n/g, "<br>");
-        let coverImg = res_json.data.book_info.thumb_uri;
+        let bookname = res_json.data.original_book_name;
+        let bookid = res_json.data.book_id;
+        let description = res_json.data.abstract.replace(/\n/g, "<br>");
+        let coverImg = res_json.data.thumb_uri;
         coverImg = "http://p3-novel.byteimg.com/origin/" + coverImg + ".png";
         return Response.success({
             name: bookname,
