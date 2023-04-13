0e645fe65a31d733d2b3e664612c90f16e032a85
PGV <nhocconsr@gmail.com>
2023-04-13 18:53:37 +0700

    fix hotruyen

--- hotruyen/src/toc.js
+++ hotruyen/src/toc.js
@@ -1,16 +1,15 @@
 function execute(url) {
     const slug = url.split('/')[4];
-    var doc = Http.get('https://hotruyen.com/muc-luc/'+slug).html();
+    var doc = fetch('https://hotruyen.com/muc-luc-sv/'+slug).html();
     var el = doc.select(".chapter")
     const data = [];
     for (var i = 12;i < el.size(); i++) {
         var e = el.get(i);
         data.push({
             name: e.text(),
-            url: e.attr("href").replace('..',''),
+            url: e.select('a').attr('href').replace('..',''),
             host: "https://hotruyen.com"
         })
     }
-
     return Response.success(data);
 }
