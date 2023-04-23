32f4e214a0542a83ede2a3532b35d33b50ce1ec8
PGV <nhocconsr@gmail.com>
2023-04-23 19:40:09 +0700

    fix mangaforest

--- mangabuddy/src/chap.js
+++ mangabuddy/src/chap.js
@@ -2,11 +2,11 @@ function execute(url) {
     let response = fetch(url);
     if(response.ok){
         let doc = response.text()
-        let server = doc.match('mainServer = "(.+)"')[1].replace('//','https://');
+        //let server = doc.match('mainServer = "(.+)"')[1].replace('//','https://');
         let allImg = doc.match("chapImages =.'(.+)'")[1].split(',');
-        let data = [];
-        allImg.forEach(item => data.push(server+item))
-        return Response.success(data);
+        // let data = [];
+        // allImg.forEach(item => data.push(server+item))
+        return Response.success(allImg);
     }
     return null;
 }
