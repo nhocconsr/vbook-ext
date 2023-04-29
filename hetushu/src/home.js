10fb51081fc9c0f40e65546818675b6390bb2139
PGV <nhocconsr@gmail.com>
2023-04-29 18:33:46 +0700

    add hetushu

--- hetushu/src/home.js
+++ hetushu/src/home.js
@@ -1,6 +1,6 @@
 function execute() {
     return Response.success([
-        { title: "全部小说", input: "0", script: "zen.js" },
+        { title: "全部小说", input: "0", script: "gen.js" },
         { title: "连载", input: "1", script: "gen.js" },
         { title: "完本", input: "2", script: "gen.js" },
     ]);
