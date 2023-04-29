f5cafb02e9d55066bf4f9da7176fb77344ece28e
PGV <nhocconsr@gmail.com>
2023-04-29 18:35:05 +0700

    remove wusanzw

--- wusanzw/src/chap.js
+++
@@ -1,8 +0,0 @@
-function execute(url) {
-    var doc = Http.get(url).html();
-    doc.select('#content p').last().remove();
-    var htm = doc.select("#content").html();
-    htm = htm.replace(/<a[^>]*>([^<]+)<\/a>/g,'');
-    htm = htm.replace(/&(nbsp|amp|quot|lt|gt);/g, "");
-    return Response.success(htm);
-}
