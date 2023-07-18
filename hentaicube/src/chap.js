2096982d89a481e459335fc0dc65c9c5795a1706
PGV <nhocconsr@gmail.com>
2023-07-18 20:09:19 +0700

    fix hentaicube

--- hentaicube/src/chap.js
+++ hentaicube/src/chap.js
@@ -2,7 +2,7 @@ function execute(url) {
     var doc = fetch(url).html();
     var el = doc.select(".doc-truyen img");   
     var data = [];
-    for (var i = 0; i < el.size() - 2; i++) {
+    for (var i = 0; i < el.size(); i++) {
         var e = el.get(i);
         data.push(e.attr("src"));     
     }
