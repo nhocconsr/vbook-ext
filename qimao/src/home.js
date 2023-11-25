11a34f9600dc9fa1965ed86f8c489c41a7dec2f9
Đường đen <79037079+duongden@users.noreply.github.com>
2023-11-25 14:08:16 +0700

    update genre

--- qimao/src/home.js
+++ qimao/src/home.js
@@ -1,7 +1,8 @@
 function execute() {
     return Response.success([
-        { title: "最新", input: "a", script: "gen.js" },
-        { title: "女生原创", input: "1", script: "gen.js" },
-        { title: "男生原创", input: "0", script: "gen.js" },
+        { title: "最新", input: "a-a-a", script: "gen.js" },
+        { title: "女生原创", input: "1-a-a", script: "gen.js" },
+        { title: "男生原创", input: "0-a-a", script: "gen.js" },
+        { title: "出版图书", input: "2-a-a", script: "gen.js" }
     ]);
 }
